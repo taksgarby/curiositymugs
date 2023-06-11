bb3a5751f55992a901be667326e9412226ebafa9
Takako Onozuka <onozukatakako@gmail.com>
2023-06-11 22:43:46 +0100

    added haunted mansion mug

--- src/data/ShopItems.js
+++ src/data/ShopItems.js
@@ -8,7 +8,8 @@ const storedItems = [
     { id: 7, name: "Tsunami Art Cup", brand: "Cup", image: 'tsunami_cup.png', price: 20 },
     { id: 8, name: "Island Mug", brand: "Mug", image: 'island_mug.png', price: 30 },
     { id: 9, name: "Modern Art Cup", brand: "Cup", image: 'modern_art_yellow.png', price: 22 },
-    { id: 10, name: "Rainy Tokyo Cup", brand: "Cup", image: 'rainy_tokyo.jpg', price: 22}
+    { id: 10, name: "Rainy Tokyo Cup", brand: "Cup", image: 'rainy_tokyo.jpg', price: 22},
+    { id: 11, name: "Haunted Mansion Mug", brand: "Mug", image: "haunted_mansion.png", price: 35}
 
 ];
 

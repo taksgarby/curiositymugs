19303235b46f4db901a0c32fa4a5f243b3473107
Takako Onozuka <onozukatakako@gmail.com>
2023-06-11 12:49:12 +0100

    changed basket text into an icon for mobile

--- src/components/Header2.js
+++ src/components/Header2.js
@@ -2,25 +2,9 @@ import { Link } from "react-router-dom";
 import '../styles/Header.css'
 import React, { useEffect, useState } from "react";
 import styled, { createGlobalStyle } from "styled-components";
-import { Home, Bookmark, User } from "react-feather";
+import FeatherIcon from 'feather-icons-react';
 
 const Header2 = ( { user } ) => {
-
-  const Header = styled.header`
-    background: #347474; 
-    color: white;
-    display: flex;
-    justify-content: space-between;
-
-  `
-
-  const HeaderGroup = styled.hgroup `
-    display: flex;
-    flex-direction: column;
-    margin-left: 2rem;
-    margin-bottom: 0.7rem;
-    padding-left: 30px;
-  `
 
   const [windowDimension, setWindowDimension] = useState(null);
 
@@ -38,37 +22,40 @@ const Header2 = ( { user } ) => {
   }, []);
 
   const isMobile = windowDimension <= 640;
+
   const Navbar = {
     Wrapper: styled.nav`
     flex: 1;
-  
     align-self: flex-start;
-  
     padding: 1rem 3rem;
-  
     display: flex;
     justify-content: space-between;
     align-items: center;
-  
     background-color: #347474;
     `,
+
     Logo: styled.header`
     color: #F6EBDA;
     padding: 0.5rem 1rem;
     font-size: 3rem;
-  `,
-  Items: styled.ul`
-  display: flex;
-  list-style: none;
-  `,
-  Item: styled.li`
-  padding: 0 1rem;
-  cursor: pointer;
-  color: #F6EBDA;
-  font-size: 2rem;
-  `,
+    `,
+
+    Items: styled.ul`
+    display: flex;
+    list-style: none;
+    `,
   
+    Item: styled.li`
+    padding: 0 1rem;
+    cursor: pointer;
+    color: #F6EBDA;
+    font-size: 1.5rem;
+    text-decoration: none;
+    `,
+
+
   };
+
   const MobileNavbar = {
     Wrapper: styled(Navbar.Wrapper)`
       align-self: flex-end;
@@ -100,22 +87,22 @@ const Header2 = ( { user } ) => {
         <MobileNavbar.Wrapper>
           <MobileNavbar.Items>
             <MobileNavbar.Item>
-              <MobileNavbar.Icon>
+              {/* <MobileNavbar.Icon>
                 <Home size={16} />
-              </MobileNavbar.Icon>
+              </MobileNavbar.Icon> */}
               Home
             </MobileNavbar.Item>
             <MobileNavbar.Item>
-              <MobileNavbar.Icon>
+              {/* <MobileNavbar.Icon>
                 <Bookmark size={16} />
-              </MobileNavbar.Icon>
-              Basket
+              </MobileNavbar.Icon> */}
+              <Link to="/curiositymugs/basket"><FeatherIcon icon="shopping-cart" /></Link>
             </MobileNavbar.Item>
             <MobileNavbar.Item>
-              <MobileNavbar.Icon>
+              {/* <MobileNavbar.Icon>
                 <User size={16} />
-              </MobileNavbar.Icon>
-              About
+              </MobileNavbar.Icon> */}
+              Items: { user.basket.length }
             </MobileNavbar.Item>
           </MobileNavbar.Items>
         </MobileNavbar.Wrapper>
@@ -127,7 +114,12 @@ const Header2 = ( { user } ) => {
           <Navbar.Logo>Curiosity Mugs</Navbar.Logo>
           <Navbar.Items>
             <Navbar.Item>Home</Navbar.Item>
-            <Navbar.Item>Basket</Navbar.Item>
+            <Navbar.Item>
+                <Link to="/curiositymugs/basket" style={{ textDecoration: 'none' }}>
+                Basket
+          
+                </Link>
+            </Navbar.Item>
             <Navbar.Item>Items: { user.basket.length }</Navbar.Item>
           </Navbar.Items>
         </Navbar.Wrapper>

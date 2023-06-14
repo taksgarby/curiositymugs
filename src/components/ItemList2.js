66d3b90a57f0d724e6369b0c564444195c180015
Takako Onozuka <onozukatakako@gmail.com>
2023-06-14 14:30:21 +0100

    changed layout for top section

--- src/components/ItemList2.js
+++ src/components/ItemList2.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import Item from './Item';
 import styled from 'styled-components';
-import TopImg from '../images/TopImg.png';
-import { Link } from "react-router-dom";
+import Rabbit from '../images/rabbit.png';
 
 const ItemsList = ({ items, user, onBasketAdd }) => {
 
@@ -19,56 +18,33 @@ const ItemsList = ({ items, user, onBasketAdd }) => {
   const TopSection = styled.div`
     background: #F6EBDA;
     margin: 0;
-    display: flex;
-    position: relative;
-    flex-flow: row wrap;
-    justify-content: space-evenly;
-    padding: 1.5rem;
   `
   const WelcomeText = styled.h1`
+    padding: 1rem;
+    text-align: center;
     color: #115052;
     font-family: "Helvetica Neue";
   `
   const WelcomeSubText = styled.h1`
+   
+    text-align: center;
     color: #e34d4d;
     font-family: "Roboto";
     font-style: italic;
+    padding-bottom: 1rem;
+    
   `
-  const UserInfo =styled.p`
-    color: #115052;
-    text-align: right;
-    font-size: 1.4rem;
-    vertical-align: bottom;
+  const TopImage = styled.div`
+    background-image: url(${Rabbit});
+    height: 200px;
+    width: 200px;
+    border-radius: 50%;
+    background-repeat: no-repeat;
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    margin-top: 2rem;
   `
-  const LinkStyle = {
-    textDecoration: "none",
-    color: "#e34d4d"
-
-  };
-  // const TopImage = styled.div`
-  //   background-image: url(${TopImg});
-  //   height: 200px;
-  //   width: 200px;
-  //   border-radius: 50%;
-  //   background-repeat: no-repeat;
-  //   display: block;
-  //   margin-left: auto;
-  //   margin-right: auto;
-  //   margin-top: 2rem;
-  // `
-
- const TopImage = styled.img`
-   width: 180px; /**/
-   height: 180px; /**/
-   border-radius: 50%;
-   display: block;
- 
- `
- const TopTexts = styled.div`
-   margin-left: 2rem;
- `
-
-
   
   const ItemIntro = styled.h1`
     color: #202020;
@@ -82,16 +58,11 @@ const ItemsList = ({ items, user, onBasketAdd }) => {
   return (
     <>
     <TopSection>
-
-    <TopTexts>
+    <TopImage src={require("../images/rabbit.png")} alt="rabbit"/>
     <WelcomeText>Welcome to Curiosity Mugs </WelcomeText>
     <WelcomeSubText> We invite you to indulge in the art of exquisite mugs and cups</WelcomeSubText>
-
-    <UserInfo>You have { user.basket.length } items in your <Link to="/curiositymugs/basket" style={LinkStyle}>basket</Link>. </UserInfo>
-    </TopTexts>
-    <TopImage src={require("../images/TopImg.png")} alt="rabbit"/>
+     
     </TopSection>
-
     <ItemIntro>Our products</ItemIntro>
     <ItemList>
    
